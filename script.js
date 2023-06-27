21193588e7e3d59f72149b43f0923cac8c61f196
Abdimalik11 <ilkacaze5522@gmail.com>
2023-06-27 13:37:04 -0700

    this is the answer of broken-keys

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // Good Luck 💪🏾
+function brokenKeys(correctPhrase, typedPhrase) {
+    let brokenKeysArr = [];
+
+    correctPhrase = correctPhrase.toLowerCase();
+    typedPhrase = typedPhrase.toLowerCase();
+  
+    for (let i = 0; i < correctPhrase.length; i++) {
+      if (correctPhrase[i] !== typedPhrase[i] && !brokenKeysArr.includes(correctPhrase[i])) {
+        brokenKeysArr.push(correctPhrase[i]);
+      }
+    }
+  
+    return brokenKeysArr;
+  }
+  console.log(brokenKeys("fikrcamp bootcamp", "fikrcomp boorcamp"));
+  console.log(brokenKeys("good night", "gppd night")); 
